61781389ae477b55a83d6c8eae50df2a747e2323
Riz Ahmed <rizwan_ahmed21@hotmail.com>
2016-01-25 00:53:43 -0800

    allow DHCP and DNS, drop packets in/out of port 0 and weird packets like all flags, accounting rules for ssh/www

--- personalF.sh
+++ personalF.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#user defined section
 #shell vars
 IPT="iptables"
 ETH="eth0"
@@ -30,11 +29,6 @@ then
   exit 0
 fi
 
-#accounting rules
-#track www
-#track ssh
-#versus rest of traffic on system
-
 #firewall rules do not touch pls
 #remove any existing rules from all chains
 $IPT --flush
@@ -57,6 +51,41 @@ $IPT -t mangle --policy OUTPUT ACCEPT
 $IPT --policy INPUT DROP
 $IPT --policy OUTPUT DROP
 $IPT --policy FORWARD DROP
+
+#user defined chains to implement accounting rules for www and ssh
+$IPT -N track-inbound
+$IPT -N track-outbound
+
+#drop all packets coming in and out of reserved port 0
+$IPT -A INPUT -j DROP -p tcp --sport 0
+$IPT -A INPUT -j DROP -p udp --sport 0
+$IPT -A INPUT -j DROP -p tcp --dport 0
+$IPT -A INPUT -j DROP -p udp --dport 0
+
+#drop invalid or weird packets
+$IPT -A INPUT -p ALL -m state --state INVALID -j DROP
+$IPT -A INPUT -p tcp ! --syn -m state --state NEW -j DROP
+$IPT -A INPUT -p tcp --tcp-flags ALL NONE -j DROP
+$IPT -A INPUT -p tcp --tcp-flags ALL ALL -j DROP
+$IPT -A INPUT -p tcp --tcp-flags ALL FIN,URG,PSH -j DROP
+$IPT -A INPUT -p tcp --tcp-flags ALL SYN,RST,ACK,FIN,URG -j DROP
+$IPT -A INPUT -p tcp --tcp-flags SYN,RST SYN,RST -j DROP
+$IPT -A INPUT -p tcp --tcp-flags SYN,FIN SYN,FIN -j DROP
+
+#allow DHCP traffic
+$IPT -A INPUT -p UDP -s 0/0 --sport 67 --dport 68 -j ACCEPT
+$IPT -A OUTPUT -p UDP --dport 68 -m state --state NEW -j ACCEPT
+
+#allow DNS traffic
+$IPT -A OUTPUT -p TCP --dport 53 -m state --state NEW -j ACCEPT
+$IPT -A OUTPUT -p UDP --dport 53 -m state --state NEW -j ACCEPT
+
+#route stuff not dropped into accounting rules
+$IPT -A INPUT -p tcp -i $ETH -j track-inbound
+
+#allow established and related incoming traffic
+$IPT -A INPUT -p ALL -i $ETH -m state --state ESTABLISHED,RELATED -j ACCEPT
+
 #permit inbound/outbound ssh packets
 $IPT -A INPUT -i $ETH -p tcp --dport 22 -m state  --state NEW,ESTABLISHED -j ACCEPT
 $IPT -A OUTPUT -o $ETH -p tcp --sport 22 -m state --state ESTABLISHED -j ACCEPT
@@ -66,3 +95,24 @@ $IPT -A OUTPUT -p tcp -m multiport --dport 80,443 -j ACCEPT
 #permit inbound/outbound for apache server
 $IPT -A INPUT -p tcp -m multiport --dport 80,443 -j ACCEPT
 $IPT -A OUTPUT -p tcp -m multiport --sport 80,443 -j ACCEPT
+
+#accounting for HTTP and SSH
+#for the input chain
+$IPT -A track-inbound -p tcp -s 0/0 --dport 80 --sport 1024:65535 -j ACCEPT
+$IPT -A track-inbound -p tcp -s 0/0 --dport 22 -j ACCEPT
+#for the forward chain
+$IPT -A FORWARD -i $ETH -m tcp -p TCP --dport 22 -j track-inbound
+$IPT -A FORWARD -i $ETH -m tcp -p TCP --dport 80 -j track-inbound
+$IPT -A FORWARD -i $ETH -m tcp -p TCP --sport 22 -j track-inbound
+$IPT -A FORWARD -i $ETH -m tcp -p TCP --sport 80 -j track-inbound
+$IPT -A FORWARD -s 0/0 -m tcp -p TCP --dport 22 -j track-outbound
+$IPT -A FORWARD -s 0/0 -m tcp -p TCP --dport 80 -j track-outbound
+$IPT -A FORWARD -s 0/0 -m tcp -p TCP --sport 22 -j track-outbound
+$IPT -A FORWARD -s 0/0 -m tcp -p TCP --sport 80 -j track-outbound
+#for the output chain
+$IPT -A track-outbound -p TCP --sport 80 -j ACCEPT
+$IPT -A track-outbound -p TCP --dport 80 -j ACCEPT
+$IPT -A track-outbound -p TCP --sport 22 -j ACCEPT
+$IPT -A track-outbound -p TCP --dport 22 -j ACCEPT
+#the rest of outbound traffic goes to accounting rules
+$IPT -A OUTPUT -p tcp -s 0/0 -j track-outbound
